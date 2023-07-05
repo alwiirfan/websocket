eb6bf8cb3982726b6c74be92f28b7dbcca2c8537
Mohammad Alwi Irfani <alwi.irfani1927@gmail.com>
2023-07-05 23:05:22 +0700

    feat: add func connect to database

---
+++ database/database.go
@@ -0,0 +1,28 @@
+package database
+
+import (
+	"database/sql"
+
+	_ "github.com/lib/pq"
+)
+
+type Database struct {
+	DB *sql.DB
+}
+
+func NewDatabase() (*Database, error) {
+	db, err := sql.Open("postgres", "postgresql://root:password@localhost:5433/go-chat?sslmode=disable")
+	if err != nil {
+		return nil, err
+	}
+
+	return &Database{DB: db}, err
+}
+
+func (database *Database) Close() {
+	database.DB.Close()
+}
+
+func (database *Database) GetDatabase() *sql.DB {
+	return database.DB
+}
