60c48673a39a073b68a34ad826f4327105dddf44
Mohammad Alwi Irfani <alwi.irfani1927@gmail.com>
2023-07-05 23:07:40 +0700

    feat: add handler to user

---
+++ internal/user/user_handler.go
@@ -0,0 +1,33 @@
+package user
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Handler struct {
+	Service
+}
+
+func NewHandler(service Service) *Handler {
+	return &Handler{
+		Service: service,
+	}
+}
+
+func (handler *Handler) CreateUser(ctx *gin.Context) {
+	var request CreateUserRequest
+	if err := ctx.ShouldBindJSON(&request); err != nil {
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	res, err := handler.Service.CreateUser(ctx.Request.Context(), &request)
+	if err != nil {
+		ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, res)
+}
