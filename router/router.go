8adfc0dee0cd7331a1b5cf8d514b000eb8fc47da
Mohammad Alwi Irfani <alwi.irfani1927@gmail.com>
2023-07-06 22:43:07 +0700

    docs: add router for create the room

--- router/router.go
+++ router/router.go
@@ -2,18 +2,23 @@ package router
 
 import (
 	"server/internal/user"
+	"server/internal/websocket"
 
 	"github.com/gin-gonic/gin"
 )
 
 var router *gin.Engine
 
-func InitRoute(userHandler *user.Handler) {
+func InitRoute(userHandler *user.Handler, websocketHandler *websocket.Handler) {
 	router = gin.Default()
 
+	// authentication
 	router.POST("/signup", userHandler.CreateUser)
 	router.POST("login", userHandler.Login)
 	router.GET("/logout", userHandler.Logout)
+
+	// websocket router
+	router.POST("/websocket/createRoom", websocketHandler.CreateRoom)
 }
 
 func Start(addr string) error {
